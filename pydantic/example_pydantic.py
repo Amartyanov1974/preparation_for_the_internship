d2379bd390898b4764019dbaee5d7e40169b4df3
Александр Мартьянов <randomquake@yandex.ru>
2024-07-25 00:36:05 +0400

    Изменил схему валидации на вложенную

--- pydantic/example_pydantic.py
+++ pydantic/example_pydantic.py
@@ -10,17 +10,30 @@ class User(BaseModel):
     service_number: int
     email: str
 
-    @field_validator("login")
+    @field_validator('login')
     def validate_login(cls, value):
         if not re.fullmatch(r'[a-zA-Z]+[0-9?]+', value):
-            raise ValueError("Login is invalid")
+            raise ValueError('Login is invalid')
         return value
 
-    @field_validator("email")
+    @field_validator('email')
     def validate_email(cls, value):
         if not re.fullmatch(r'[a-zA-Z0-9.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+', value):
-            raise ValueError("Email is invalid")
+            raise ValueError('Email is invalid')
         return value
+
+
+class Boss(BaseModel):
+    user: User
+    office: str
+
+    @field_validator('office')
+    def validate_office(cls, value):
+        if not re.fullmatch(r'^[А-Я]{1}[а-я]{4,}[а-я ]{6,}', value):
+            raise ValueError('Office is invalid')
+        return value
+
+
 
 
 def main():
@@ -39,10 +52,14 @@ def main():
     }
 
     try:
-        valid_result = User(**valid_user)
-        print(valid_result.model_dump_json(indent=4))
-        invalid_result = User(**invalid_user)
-        print(invalid_result.model_dump_json(indent=4))
+        valid_user_result = User(**valid_user)
+        valid_boss_result = Boss(user=valid_user_result, office = 'Отдел кадров')
+        print(valid_boss_result.model_dump_json(indent=4))
+        invalid_user_result = User(**invalid_user)
+    except ValueError as e:
+        pprint(e.errors(), indent=4)
+    try:
+        invalid_boss_result = Boss(user=valid_user_result, office = 'Отдел')
     except ValueError as e:
         pprint(e.errors(), indent=4)
 
@@ -52,26 +69,33 @@ if __name__ == '__main__':
     """
     Вывод без ошибки:
     {
-        "login": "login22",
-        "service_number": 2000,
-        "email": "name@domen.ru"
+        "user": {
+            "login": "login22",
+            "service_number": 2000,
+            "email": "name@domen.ru"
+        },
+        "office": "Отдел кадров"
     }
 
-    Вывод ошибки:
-    [
-        {
-            "loc": [
-                "login"
-            ],
-            "msg": "Login is invalid",
-            "type": "value_error"
-        },
-        {
-            "loc": [
-                "email"
-            ],
-            "msg": "Email is invalid",
-            "type": "value_error"
-        }
-    ]
+    Вывод ошибки User:
+    [   {   'ctx': {'error': ValueError('Login is invalid')},
+            'input': 'login_2',
+            'loc': ('login',),
+            'msg': 'Value error, Login is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
+        {   'ctx': {'error': ValueError('Email is invalid')},
+            'input': 'name_domen.ru',
+            'loc': ('email',),
+            'msg': 'Value error, Email is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'}]
+
+    Вывод ошибки Boss:
+    [   {   'ctx': {'error': ValueError('Office is invalid')},
+            'input': 'Отдел',
+            'loc': ('office',),
+            'msg': 'Value error, Office is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'}]
     """
