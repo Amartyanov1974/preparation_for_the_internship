d1a491fe5bf3b0ea0098b9d04b9613aa25b53f6f
Amartyanov1974 <randomquake@yandex.ru>
2024-07-25 16:34:50 +0400

    Update tests.py

--- py_test/tests.py
+++ py_test/tests.py
@@ -7,7 +7,7 @@ from simple_db.models import Thing
 
 @pytest.mark.django_db
 def test_get_thing():
-    # Раскоменторовать, чтобы вызвать ошибку
+    # Раскомментировать, чтобы вызвать ошибку
     # thing = Thing.objects.create(name='One_Thing', amount=3)
     with pytest.raises(ObjectDoesNotExist):
         thing = Thing.objects.get(name='One_Thing')
