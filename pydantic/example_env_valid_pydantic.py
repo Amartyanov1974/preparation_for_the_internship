419c8278c47a721a838152f7b23df3511e7e46ec
Amartyanov1974 <randomquake@yandex.ru>
2024-07-25 14:23:35 +0400

    добавил валидацию переменных окружения с помощью pydantic_settings

--- pydantic/example_env_valid_pydantic.py
+++ pydantic/example_env_valid_pydantic.py
@@ -1,18 +1,39 @@
+from os import environ
 import json
 from pprint import pprint
 import re
 
-from environs import Env
-from pydantic import BaseModel, field_validator
+
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class User(BaseModel):
+
+
+
+class SubParams(BaseSettings):
+    first_parameter: str
+    second_parameter: str
+
+    @field_validator('second_parameter')
+    def validate_latin_text(cls, value):
+        if not re.fullmatch(r'[a-zA-Z ]+', value):
+            raise ValueError('second_parameter is invalid')
+        return value
+
+    @field_validator('first_parameter')
+    def validate_cyril_text(cls, value):
+        if not re.fullmatch(r'[а-яА-ЯёЁ ]+', value):
+            raise ValueError('first_parameter is invalid')
+        return value
+
+
+class Params(BaseSettings):
     latin_text: str
     cyril_text: str
     email: str
-    """
-    Вложенная схема валидации данных
-    """
+    more_params: SubParams
+
     @field_validator('latin_text')
     def validate_latin_text(cls, value):
         if not re.fullmatch(r'[a-zA-Z]+', value):
@@ -33,94 +54,90 @@ class User(BaseModel):
 
 
 def main():
-    env = Env()
-    env.read_env()
-    """
-    В файле .env содержатся следующие переменные среды:
-
-    LATIN_TEXT_VAL=qwertyuiop
-    CYRIL_TEXT_VAL=йцукенгшфывапр
-    EMAIL_VAL=to.email@email.com
-    LATIN_TEXT_INVAL=qwertyuiopй
-    CYRIL_TEXT_INVAL=йцукенгшфывапрz
-    EMAIL_INVAL=to.email-email.com
 
     """
-    latin_text = env('LATIN_TEXT_VAL')
-    cyril_text = env('CYRIL_TEXT_VAL')
-    email = env('EMAIL_VAL')
+    В файле val.env содержатся следующие переменные среды:
 
-    user_values = {
-        'latin_text': 'LatinText',
-        'cyril_text': 'ТекстКирилицей',
-        'email': 'new.mail@mail.com',
-        }
-    user_values = json.dumps(user_values, indent=4, ensure_ascii=False)
-    # Прочитать данные из JSON с помощью Pydantic
-    valid_result = User.parse_raw(user_values)
-    print(valid_result.model_dump_json(indent=4))
+    LATIN_TEXT=qwertyuiop
+    CYRIL_TEXT=йцукенгшфывапр
+    EMAIL=to.email@email.com
 
+    В файле inval.env содержатся следующие переменные среды:
+    LATIN_TEXT=qwertyuiopй
+    CYRIL_TEXT=йцукенгшфывапрz
+    EMAIL=to.email-email.com
     """
-    Вывод:
-    {
-        "latin_text": "LatinText",
-        "cyril_text": "ТекстКирилицей",
-        "email": "new.mail@mail.com"
-    }
-    """
+
+    environ['first_parameter'] = 'Первый параметр'
+    environ['second_parameter'] = 'Second parameter'
+    params = Params(_env_file='val.env', _env_file_encoding='utf-8', more_params=SubParams())
+    print(params.model_dump_json(indent=4))
 
     try:
-        valid_result = User(
-            latin_text=latin_text,
-            cyril_text=cyril_text,
-            email=email,
-            )
-        # Экспортировать данные в формат JSON с помощью Pydantic
-        print(valid_result.model_dump_json(indent=4))
-        """
-        Вывод:
-        {
-            "latin_text": "qwertyuiop",
-            "cyril_text": "йцукенгшфывапр",
-            "email": "to.email@email.com"
-        }
-        """
-
-        latin_text = env('LATIN_TEXT_INVAL')
-        cyril_text = env('CYRIL_TEXT_INVAL')
-        email = env('EMAIL_INVAL')
-
-        invalid_result = User(
-            latin_text=latin_text,
-            cyril_text=cyril_text,
-            email=email,
-            )
-        print(invalid_result.json())
-
+        params = Params(_env_file='inval.env', _env_file_encoding='utf-8', more_params=SubParams())
     except ValueError as e:
         pprint(e.errors(), indent=4)
-        """
-        Вывод:
-        [   {   'ctx': {'error': ValueError('latin_text is invalid')},
-                'input': 'qwertyuiopй',
-                'loc': ('latin_text',),
-                'msg': 'Value error, latin_text is invalid',
-                'type': 'value_error',
-                'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
-            {   'ctx': {'error': ValueError('cyril_text is invalid')},
-                'input': 'йцукенгшфывапрz',
-                'loc': ('cyril_text',),
-                'msg': 'Value error, cyril_text is invalid',
-                'type': 'value_error',
-                'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
-            {   'ctx': {'error': ValueError('Email is invalid')},
-                'input': 'to.email-email.com',
-                'loc': ('email',),
-                'msg': 'Value error, Email is invalid',
-                'type': 'value_error',
-                'url': 'https://errors.pydantic.dev/2.6/v/value_error'}]
-        """
+
+
+    environ['first_parameter'] = 'Первый параметр9'
+    environ['second_parameter'] = 'Second parameter9'
+
+    try:
+        params = Params(_env_file='inval.env', _env_file_encoding='utf-8', more_params=SubParams())
+    except ValueError as e:
+        pprint(e.errors(), indent=4)
+
 
 
 if __name__ == '__main__':
     main()
+
+
+    """
+    Вывод без ошибки:
+
+    {
+        "latin_text": "qwertyuiop",
+        "cyril_text": "йцукенгшфывапр",
+        "email": "to.email@email.com",
+        "more_params": {
+            "first_parameter": "Первый параметр",
+            "second_parameter": "Second parameter"
+        }
+    }
+
+    Вывод ошибки в Params:
+    [   {   'ctx': {'error': ValueError('latin_text is invalid')},
+            'input': 'qwertyuiopй',
+            'loc': ('latin_text',),
+            'msg': 'Value error, latin_text is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
+        {   'ctx': {'error': ValueError('cyril_text is invalid')},
+            'input': 'йцукенгшфывапрz',
+            'loc': ('cyril_text',),
+            'msg': 'Value error, cyril_text is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
+        {   'ctx': {'error': ValueError('Email is invalid')},
+            'input': 'to.email-email.com',
+            'loc': ('email',),
+            'msg': 'Value error, Email is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'}]
+
+    Вывод ошибки в Subparams:
+
+    [   {   'ctx': {'error': ValueError('first_parameter is invalid')},
+            'input': 'Первый параметр9',
+            'loc': ('first_parameter',),
+            'msg': 'Value error, first_parameter is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'},
+        {   'ctx': {'error': ValueError('second_parameter is invalid')},
+            'input': 'Second parameter9',
+            'loc': ('second_parameter',),
+            'msg': 'Value error, second_parameter is invalid',
+            'type': 'value_error',
+            'url': 'https://errors.pydantic.dev/2.6/v/value_error'}]
+    """
